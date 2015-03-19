ccd11b2e29273bc52d93aa77496fcbe76faccf25
Patrick Shampine <pshampine@gunnjerkens.com>
2015-03-19 16:58:07 -0700

    Committed non sample conf

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+
+  'url'      => 'http://example.com/endpoint',
+  'filepath' => '/path/to/your/data/set',
+  'append'   => [
+    'some_field' => 'some data',
+  ],
+
+);
